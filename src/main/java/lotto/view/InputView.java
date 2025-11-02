39438b28b9cdadcc44f163ac1579844d632a6859
bogan <bogang2000@naver.com>
2025-11-02 22:36:50 +0900

    feat : 로또 구입 금액이 1000원 단위인지 검증 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,9 +7,12 @@ public class InputView {
 
     public int lottoMoneyInput(){
         System.out.println("구입금액을 입력해주세요.");
-        String InputMoney = Console.readLine();
-        validateIntegerInput(InputMoney);
-        return Integer.parseInt(InputMoney);
+        String inputMoney = Console.readLine();
+        validateIntegerInput(inputMoney);
+
+        int money = Integer.parseInt(inputMoney.trim());
+        validateLottoDividePrice(money);
+        return money;
     }
 
     public void validateIntegerInput(String validateString){
@@ -17,6 +20,12 @@ public class InputView {
             Integer.parseInt(validateString);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_INPUT.getMessage());
+        }
+    }
+
+    public void validateLottoDividePrice(int money){
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_DIVIDE_PRICE.getMessage());
         }
     }
 }
