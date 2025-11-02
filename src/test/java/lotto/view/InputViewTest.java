2497e63eaa9701ebdeb5d949f1b72dda965f78f7
bogan <bogang2000@naver.com>
2025-11-02 23:07:16 +0900

    test : 입력한 로또 금액 정수 검증 테스트 추가

---
+++ src/test/java/lotto/view/InputViewTest.java
@@ -0,0 +1,25 @@
+// java
+package lotto.view;
+
+import lotto.ErrorMessage;
+import org.junit.jupiter.api.Test;
+
+import static lotto.ErrorMessage.INVALID_NUMBER_INPUT;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class InputViewTest {
+
+    private final InputView inputView = new InputView();
+
+    @Test
+    void 로또_금액_정수_검증() {
+        // 정수 변환이 가능한 경우
+        assertDoesNotThrow(() -> inputView.validateIntegerInput("1000"));
+        // 정수 변환이 안되는 경우
+        assertThatThrownBy(() -> inputView.validateIntegerInput("1000a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_NUMBER_INPUT.getMessage());
+    }
+}
